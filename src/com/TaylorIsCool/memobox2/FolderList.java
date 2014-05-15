0a6b67cda892d5daec77bb0dcb5230e7fa433178
unknown <towenmilner@gmail.com>
2014-05-14 23:32:06 -0700

    Override this project with this new init

---
+++ src/com/TaylorIsCool/memobox2/FolderList.java
@@ -0,0 +1,15 @@
+package com.TaylorIsCool.memobox2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FolderList extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_folder_list);
+	}
+}
