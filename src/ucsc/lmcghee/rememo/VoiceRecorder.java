5b93d4c3483377573abc20b381cf7e26dff6006a
raimen <lmcghee.vitech@gmail.com>
2014-05-28 19:49:46 -0700

    Fixed more things

--- src/ucsc/lmcghee/rememo/VoiceRecorder.java
+++ src/ucsc/lmcghee/rememo/VoiceRecorder.java
@@ -1,5 +1,9 @@
-// VoiceRecorder.java
-// Main Activity for the VoiceRecorder class.
+/*
+ * Andrew Fisher
+ * Logan McGhee
+ * Taylor Owen
+ */
+
 package ucsc.lmcghee.rememo;
 
 import java.io.File;
@@ -7,25 +11,13 @@ import java.io.IOException;
 import java.util.Calendar;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 public class VoiceRecorder extends Activity 
 {
